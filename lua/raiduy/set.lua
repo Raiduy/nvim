60ab02f55426cc7a088f2934bcc1b2ee7b9acabe
Raiduy <radu.apsan@gmail.com>
2023-09-29 23:12:31 +0200

    finalize vim setup

--- lua/raiduy/set.lua
+++ lua/raiduy/set.lua
@@ -1,12 +1,23 @@
-vim.opt.nu = true
+vim.opt.nu = true   -- line numbers
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.wrap = false
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
 vim.opt.smartindent = true
 
 vim.opt.scrolloff = 10
 
 vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
